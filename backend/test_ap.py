0d68b8633ce855cea4ae80030ccc91b8a856fc05
Sarah Temple <68139406+SarahEliseTemple@users.noreply.github.com>
2025-10-28 15:20:50 -0700

    deleted a line in dockercomp fixed a recipe test case

--- backend/test_ap.py
+++ backend/test_ap.py
@@ -12,7 +12,7 @@ class RecipeRoutesTestCase(unittest.TestCase):
         # sample recipe data
         self.new_recipe = {
             "Title": "Test Brownies",
-            "Author": str(uuid.uuid4()),
+            "Author": "97c33e9b-e74d-425b-8700-b7aa20ff9da7",
             "Category": "Dessert",
             "MinutesToComplete": 45,
             "Description": "Rich chocolate brownies",
@@ -32,6 +32,8 @@ class RecipeRoutesTestCase(unittest.TestCase):
         )
         self.assertIn(response.status_code, [201, 500])  # 500 if DB error
         print("POST response:", response.json)
+        self.assertEqual(response.json['message'], 'Recipe created successfully')
+
 
     def test_get_all_recipes(self):
         """Test GET /recipes"""
@@ -61,6 +63,25 @@ class RecipeRoutesTestCase(unittest.TestCase):
         recipe_id = 1
         response = self.client.delete(f"/recipes/{recipe_id}")
         self.assertIn(response.status_code, [200, 404, 500])
+'''
+class UserTasksTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+        self.client.testing = True
 
+        # sample recipe data
+        self.new_user = {
+            "Title": "Test Brownies",
+            "Author": "97c33e9b-e74d-425b-8700-b7aa20ff9da7",
+            "Category": "Dessert",
+            "MinutesToComplete": 45,
+            "Description": "Rich chocolate brownies",
+            "Ingredients": ["flour", "sugar", "cocoa"],
+            "Directions": ["mix", "bake", "cool"],
+            "DietaryRestrictions": "Vegetarian",
+            "DateCreated": "2025-10-20",
+            "Likes": 0
+        }
+'''
 if __name__ == '__main__':
     unittest.main()
