67463b1a41600bd8120115ceb4c4d111735c046e
Sarah Temple <68139406+SarahEliseTemple@users.noreply.github.com>
2025-11-12 00:24:55 -0800

    Completed backend startup to make accounts

--- backend/test_auth.py
+++ backend/test_auth.py
@@ -1,6 +1,6 @@
 
 import unittest
-from backend.user import user
+from user import user
 import json
 """
 peanuts 
@@ -11,20 +11,37 @@ dairy
 treenuts
 """
 
-
-test_user_data = {"username": 'testuser', 
-             "email": 'testuser@gmail.com', 
-             "password": 'Geeks',
-             "allergens":["peanuts", "treenuts"]}
 class UserTasksTestCase(unittest.TestCase):
     def setUp(self):
-        test_user = user(test_user_data)
+        self.test_user_data = {"username": 'testuser1', 
+             "email": "testuser1@gmail.com", 
+             "password": "GeeksforGeeks2",
+             "allergens":["peanuts", "treenuts"]}
+        self.uuid = "fce74316-e465-412b-8e57-8ff7cbd72d3d"
+
+    def test_a_insert_public_users(self):
+        test_user = user(self.test_user_data, self.uuid)
+        result = test_user.create_profile()
+        self.assertEqual(result["message"],  "Profile created successfully")
+
+    def test_b_pull_data_users(self):   
+        data = user.get_user_with_id(self.uuid)
+        self.assertEqual(data.data[0]['username'],  'testuser1')
+
+    def test_c_delete_user(self):
+        data = user.delete_profile(self.uuid)
+        print(data)
+        self.assertEqual(data.data[0]['username'],  'testuser1')
+"""
+    def test_create_profile(self):
+        test_user = user(self.test_user_data)
+        test_user.create_auth()
         test_user.create_profile()
         # check if user was created
         data = user.get_user_with_id(test_user)
         decoded = json.load(data)
-        self.assertEqual(decoded["username"], test_user_data.username)
-        
+        self.assertEqual(decoded["username"], self.test_user_data.username)
+"""   
 '''
     def test_register(self, test_user):
         """Test GET /recipes"""
