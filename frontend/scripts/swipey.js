46579f9af4040d1b64e335436f243f020ba711c8
kadeemay <kadeemayw@gmail.com>
2025-11-19 18:11:14 -0800

    working feed

--- frontend/scripts/swipey.js
+++ frontend/scripts/swipey.js
@@ -1,5 +1,5 @@
 const API_BASE = "http://localhost:5001";
-// const FEED_ENDPOINT = "/feed/sarah_test";
+
 
 const userID = localStorage.getItem("tastebuddin_user_id");
 
@@ -8,12 +8,12 @@ if (!userID) {
 }
 
 // // new fetch functionality
-// fetch(`http://localhost:5001/feed/${userID}`)
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log("User feed: ", data);
-//         // renderFeed(data.data);
-//     })
+fetch(`http://localhost:5001/feed/${userID}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log("User feed: ", data);
+        renderFeed(data.data);
+    })
 
 // let recipes = [
 //         {
@@ -58,6 +58,25 @@ let ingEl = document.querySelector("#recipe-ingredient-list");
 let dirEl = document.querySelector("#recipe-steps-list");
 let timeEl = document.querySelector("#recipe-est-time");
 
+
+function renderFeed(feedData) {
+    console.log("Rendering feed:", feedData);
+
+    if (!feedData || feedData.length === 0) {
+        showDefault();
+        return;
+    }
+
+    // Save backend feed into global recipes list
+    recipes = feedData;
+
+    // reset index
+    idx = 0;
+
+    // show first recipe
+    showRecipe();
+}
+
 async function loadRecipes() {
     // const res = await fetch(API_BASE + FEED_ENDPOINT);
     // const json = await res.json();
@@ -95,24 +114,35 @@ function showRecipe() {
 
     const r = recipes[idx];
 
-    console.log("we made it to showrecipes");
-    console.log(r);
-
     titleRef.innerHTML = r.title || "Untitled Recipe";
 
-    // display image if exists
+    // photo
     if (r.photopath) {
         imgEl.src = r.photopath;
         imgEl.style.display = "block";
     } else {
-        imgEl.style.display = "none";
+        imgEl.src = "default-resources/empty-dish.jpg";
     }
 
+    // description
     descEl.innerHTML = r.description || "(No description)";
-    ingEl.innerHTML = (r.ingredients || []).join("<br>");
-    dirEl.innerHTML = (r.directions || []).join("<br>");
-    timeEl.innerHTML = (r.minutestocomplete  + " Minutes" || "NA");
+
+    // ingredients: array → HTML list
+    ingEl.innerHTML = (r.ingredients || [])
+        .map(i => `<li>${i}</li>`)
+        .join("");
+
+    // directions: array → HTML list
+    dirEl.innerHTML = (r.directions || [])
+        .map(step => `<li>${step}</li>`)
+        .join("");
+
+    // estimated time
+    timeEl.innerHTML = r.minutestocomplete
+        ? `${r.minutestocomplete} Minutes`
+        : "N/A";
 }
+
 
 
 // TODO: make a copy, change this to "likeAnim"
