c4d80cdc5bce5f8174b1ff2e3529b3f7ab27889d
Sarah Temple <68139406+SarahEliseTemple@users.noreply.github.com>
2025-11-28 15:26:49 -0800

    working logout script and nav bar in index.html

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -2,21 +2,22 @@ const API_BASE = "http://localhost:5001";
 
 
 const userID = localStorage.getItem("tastebuddin_user_id");
+document.addEventListener("DOMContentLoaded", () => {
+    if (userID) {
+        // For someone who is logged in
+        // Hide logged out
+        document.getElementById("logged-out").style.display = "none";
+        // Show logged in
+        document.getElementById("logged-in").style.display = "block";
+        return;
 
-if (userID) {
-    // For someone who is logged in
-    // Hide logged out
-    document.getElementById("logged-out").style.display = "none";
-    // Show logged in
-    document.getElementById("logged-in").style.display = "block";
-    return;
-
-} else {
-    // For someone who is logged out
-    // Show logged out
-    document.getElementById("logged-out").style.display = "block";
-    // Hide logged in
-    document.getElementById("logged-in").style.display = "none";
-    return;
-    
-}
+    } else {
+        // For someone who is logged out
+        // Show logged out
+        document.getElementById("logged-out").style.display = "block";
+        // Hide logged in
+        document.getElementById("logged-in").style.display = "none";
+        return;
+        
+    }
+});
